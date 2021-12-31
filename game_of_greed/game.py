5f8ca5d67d4c103837a1995ae4034f0f3e5155d8
Edris <ali_berg1@yahoo.com>
2021-12-30 21:01:01 -0800

    test 3

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,10 +1,18 @@
-from random import randint
-from game_of_greed.game_logic import GameLogic
+
+from game_logic import GameLogic
+
 
 class Game:
+    def quiter(self):
+        print("Thanks for playing. You earned 0 points")
+
+    def decline(self):
+        print("OK. Maybe another time")
+
 
     def default_roller(dice=6):
         return GameLogic.roll_dice(dice)
+ 
     
     def play(self, roller=default_roller):
         print("Welcome to Game of Greed")
@@ -13,7 +21,7 @@ class Game:
         while True:
             response = input("> ")
             if response == "n" or response == "no":
-                print("OK. Maybe another time")
+                self.decline()
                 break
             elif response == "y" or response == "yes":
                 counter += 1
@@ -24,14 +32,33 @@ class Game:
                 for num in roll:
                     roller_str += str(num) + " "
                 print(f'Starting round {counter}')
-                print(f'Rolling {6} dice...')
+                print(f'Rolling {len(roll)} dice...')
 
                 print(f'*** {roller_str}***')
                 print("Enter dice to keep, or (q)uit:")
                 choice = input("> ")
                 if choice == "q":
-                    print("Thanks for playing. You earned 0 points")
-                    break
+                    self.quiter()
+                    
+                # else:
+                    
+                #     print(f"You have {50} unbanked points and 5 dice remaining")
+                #     print("(r)oll again, (b)ank your points or (q)uit:")
+                #     banked = input("> ").lower()
+                #     if  banked == "b":
+                #         print(f'You banked 50 points in round {counter}')
+                #         counter +=1
+                #         print(f'Total score is {50} points')
+                #         print(f'Starting round {counter}')
+                #         print(f'Rolling {len(roll)} dice...')
+                #         print(f'*** {roller_str}***')
+                #         print("Enter dice to keep, or (q)uit:")
+                #         choice = input("> ")
+                #     elif choice == "q":
+                #         print(f"Thanks for playing. You earned {50} points")
+                #         break
+
+                              
 
 
 if __name__ == "__main__":
@@ -41,6 +68,5 @@ if __name__ == "__main__":
     def mock_roller():
         return (4,3, 1, 1)
         # return rolls.pop(0) if rolls else Game.default_roller()
-    
     Game.play(mock_roller)
     
